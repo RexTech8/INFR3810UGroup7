d7392d14eab1e0e185c34d29e14aae9debac7254
RexTech8 <rexmartin1403@gmail.com>
2024-03-21 18:03:06 -0400

    changed folder name

--- app.py
+++ app.py
@@ -3,7 +3,7 @@
 from flask import Flask, render_template, request
 import pymysql
 
-app = Flask(__name__, template_folder="templates")
+app = Flask(__name__, template_folder="views")
 
 ##### DATABASE #####
 
